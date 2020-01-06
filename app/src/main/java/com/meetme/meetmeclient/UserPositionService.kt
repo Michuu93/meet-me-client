af1efce663691669d4cc8ea4d5bec8d3ffaaebab
DeKuczma <awid12@gmail.com>
2020-01-06 20:12:38 +0100

    Servis stay alive and do something every 5 seconds

--- app/src/main/java/com/meetme/meetmeclient/UserPositionService.kt
+++ app/src/main/java/com/meetme/meetmeclient/UserPositionService.kt
@@ -1,37 +1,44 @@
 package com.meetme.meetmeclient
 
-import android.app.IntentService
+import android.app.Service
 import android.content.Intent
 import android.os.Handler
-import java.util.*
+import android.os.IBinder
 
 
-class UserPositionService : IntentService(UserPositionService::class.simpleName) {
-    private lateinit var timer: Timer
+class UserPositionService : Service() {
 
-    override fun onHandleIntent(intent: Intent?) {
-        println("Start background UserPositionService")
-        // TODO destroy only if MapsActivity destroyed
-        val apiUrl = intent?.getStringExtra("USER_POSITION_API_URL")
+    override fun onBind(p0: Intent?): IBinder? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
-        timer = Timer(false)
-        val timerTask: TimerTask = object : TimerTask() {
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
+
+        val h = Handler()
+        val delay : Long = 5000 //milliseconds
+
+        h.postDelayed(object : Runnable {
             override fun run() {
-                Handler().post {
-                    sendUserPosition(apiUrl)
-                }
+
+                val apiUrl = intent?.getStringExtra("USER_POSITION_API_URL")
+                sendUserPosition(apiUrl)
+
+                h.postDelayed(this, delay)
             }
-        }
-        timer.schedule(timerTask, 1000)
+        }, delay)
+
+        return START_NOT_STICKY
     }
 
     override fun onDestroy() {
         println("Destroy background UserPositionService")
-        timer.cancel()
         super.onDestroy()
     }
 
     private fun sendUserPosition(apiUrl: String?) {
+
         // TODO send user position to backend api
         println("Sending user position to backend api (url = ${apiUrl})")
     }
