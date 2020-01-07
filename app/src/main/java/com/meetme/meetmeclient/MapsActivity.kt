59a40fb2e36dd1f7ee336f81ed42c9f91a1c2e33
DeKuczma <awid12@gmail.com>
2020-01-07 21:08:39 +0100

    Added requesting perrmisions for location

--- app/src/main/java/com/meetme/meetmeclient/MapsActivity.kt
+++ app/src/main/java/com/meetme/meetmeclient/MapsActivity.kt
@@ -1,25 +1,44 @@
 package com.meetme.meetmeclient
 
+import android.Manifest
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import androidx.core.app.ActivityCompat
+
+
+
+
 
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var userPositionIntent : Intent
+    private var permissionsGranted = false
+    private val permissionRequestCode = 123
+    private val permissions = arrayOf(
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
+
+        if(arePermissionsGranted())
+            permissionsGranted = true
+        else
+            requestPermissions()
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
@@ -29,7 +48,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onStart() {
         super.onStart()
-        startUserPositionBackgroundService()
+        if(permissionsGranted)
+            startUserPositionBackgroundService()
     }
 
     private fun startUserPositionBackgroundService() {
@@ -40,7 +60,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
     override fun onDestroy() {
-        println("Stoping service")
         stopService(userPositionIntent)
         super.onDestroy()
     }
@@ -52,5 +71,38 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    fun arePermissionsGranted(): Boolean {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+            && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+            return true
+        return false
+    }
+
+    fun requestPermissions() {
+        ActivityCompat.requestPermissions(this, permissions, permissionRequestCode)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        var grantedAll : Boolean = true
+        when(requestCode) {
+            permissionRequestCode -> {
+                permissions.forEach { i ->
+                    var result = 0
+                    if (grantResults.isNotEmpty()) {
+                        for (item in grantResults) {
+                            if(item == PackageManager.PERMISSION_DENIED)
+                                grantedAll = false
+                        }
+                    }
+                }
+            }
+            else -> grantedAll = false
+        }
+        if(grantedAll)
+            startUserPositionBackgroundService()
+        else
+            finish()
     }
 }
