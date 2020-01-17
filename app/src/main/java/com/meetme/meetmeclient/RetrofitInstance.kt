6957b09dc35c8c49b96c7b3620b77077456d3618
sucuncu <sucuncu@consdata.pl>
2020-01-17 18:56:10 +0100

    Add info about api url

--- app/src/main/java/com/meetme/meetmeclient/RetrofitInstance.kt
+++ app/src/main/java/com/meetme/meetmeclient/RetrofitInstance.kt
@@ -3,8 +3,9 @@ package com.meetme.meetmeclient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class RetrofitInstance{
+class RetrofitInstance {
     companion object {
+        // set server url
         var baseUrl = "http://192.168.8.102:8080/"
         val retrofit: Retrofit = Retrofit.Builder()
             .baseUrl(baseUrl)
